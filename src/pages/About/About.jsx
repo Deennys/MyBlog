94be0700f7abab388600fa6cce3396af9fb74d6c
deennys <dennys.a@partner.samsung.com>
2023-09-04 11:59:02 -0300

    finished About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
 import styles from './About.module.css';
 
 export default function About() {
   return (
-    <div>
-        <h1>About</h1>
+    <div className={styles.about}>
+        <h2>About My <span>Blog</span></h2>
+        <p>This project consists of a blog made with React on the Front-end and Firebase on the Back-end</p>
+        <Link to="/post/create" className='btn'>
+          Create Post
+        </Link>
     </div>
   )
 }
