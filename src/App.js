e7944075ae6055ffbb325d148b6797533bb21c7e
deennys <dennys.a@partner.samsung.com>
2023-09-13 11:23:45 -0300

    Created post.jsx

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import Login from "./pages/Login/Login";
 import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Search from "./pages/Search/Search";
+import Post from "./pages/Post/Post";
 
 //context
 import { AuthProvider } from "./context/AuthContext";
@@ -52,6 +53,7 @@ export default function App() {
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/search" element={<Search />}/>
+              <Route path="/posts/:id" element={<Post />}/>
               <Route path="/login" element={user ? <Navigate to="/"/> : <Login />} />
               <Route path="/register" element={user ? <Navigate to="/"/> : <Register />} />
               <Route path="/posts/create" element={user ? <CreatePost /> : <Navigate to="/login"/>} />
